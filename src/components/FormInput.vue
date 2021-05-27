f40b5c4701e6aa1cd697489190ea72f55932c6b8
Thang <thangtd@cmcinfosec.com>
2021-05-27 14:24:10 +0700

    add table list

--- src/components/FormInput.vue
+++ src/components/FormInput.vue
@@ -2,10 +2,10 @@
   <div class="content">
     <div class="container">
       <div class="row align-items-stretch justify-content-center no-gutters">
-        <div class="col-md-7">
+        <div class="col-md-8">
           <div class="form h-100 contact-wrap p-5">
-            <h3 class="text-center mb-5">Router Reboot</h3>
-            <form class="mb-5" @submit="checkForm">
+            <h3 class="text-center mb-4">Router Reboot</h3>
+            <form class="mb-3">
               <div class="row">
                 <div class="col-md-6 form-group mb-3">
                   <label for="" class="col-form-label">IP</label>
@@ -40,7 +40,7 @@
                 </div>
               </div>
 
-              <div class="row mb-5">
+              <div class="row">
                 <div class="col-md-6 form-group" style="height: 80px">
                   <input type="checkbox" v-model="isSpecificTime" />
                   <label
@@ -75,14 +75,18 @@
                 </div>
               </div>
               <div class="row justify-content-center">
-                <div class="col-md-5 form-group text-center">
+                <div class="col-md-5 form-group text-center mb-5">
                   <input
                     type="submit"
                     value="Reboot"
                     class="btn btn-block btn-primary rounded-0 py-2 px-4"
+                    @click="checkForm"
                   />
-                  <span class="submitting"></span>
                 </div>
+              </div>
+              <div class="text-center">
+                <vue-good-table :columns="columns" :rows="listScheduler"
+                max-height="300px" :fixed-header="true" :search-options="tableSearch" :pagination-options="tablePagination" />
               </div>
             </form>
 
@@ -108,9 +112,10 @@ import DatePicker from 'vue2-datepicker'
 import 'vue2-datepicker/index.css'
 import Loading from 'vue-loading-overlay'
 import 'vue-loading-overlay/dist/vue-loading.css'
+import { VueGoodTable } from 'vue-good-table'
 
 export default {
-  components: { Loading, DatePicker },
+  components: { Loading, DatePicker, VueGoodTable },
   name: 'FormInput',
   data () {
     return {
@@ -121,8 +126,32 @@ export default {
       time: '',
       server_ip: 'http://103.229.42.221:9498',
       isSpecificTime: false,
-      isLoading: false
+      isLoading: false,
+      listScheduler: [],
+      columns: [
+        {
+          label: 'IP',
+          field: 'ip'
+        },
+        {
+          label: 'Schedule',
+          field: 'schedule'
+        }
+      ],
+      tableSearch: {
+        enabled: true,
+        placeholder: 'Search this table'
+      },
+      tablePagination: {
+        enabled: true,
+        mode: 'pages',
+        perPage: 5,
+        perPageDropdown: [5, 10, 15]
+      }
     }
+  },
+  mounted () {
+    this.getListSchedule()
   },
   methods: {
     checkForm (e) {
@@ -146,6 +175,7 @@ export default {
               type: 'error'
             })
           } else {
+            this.getListSchedule()
             this.$notify({
               group: 'form',
               title: response.data,
@@ -162,6 +192,11 @@ export default {
           })
         })
       e.preventDefault()
+    },
+    getListSchedule () {
+      axios.get(`${this.server_ip}/list_scheduler`).then((response) => {
+        this.listScheduler = response.data
+      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Notifications from 'vue-notification'
+import VueGoodTablePlugin from 'vue-good-table'
 import '../fonts/icomoon/style.css'
 import '../css/bootstrap.min.css'
 import '../css/style.css'
-import Notifications from 'vue-notification'
+import 'vue-good-table/dist/vue-good-table.css'
+
+Vue.use(VueGoodTablePlugin)
 
 Vue.use(Notifications)
 /* eslint-disable no-new */
